dffca80f98b5d7d4af1d699d59666a0284668dc7
kristianinc <xankiiza@gmail.com>
2023-06-23 02:24:38 -0400

    editing constraint changed back

--- app/Http/Controllers/insert_data.php
+++ app/Http/Controllers/insert_data.php
@@ -323,7 +323,7 @@ public Function insert_team(Request $request){
                 'role' => $request->role,
                 'bio' => $request->bio,
                 'photo' => $picname,
-                'registered_by' => 2,
+                'registered_by' => $admin,
                 'created_at' => now(),
                 'updated_at' => now(),        
                   
