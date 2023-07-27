9a59985f5a4624ab73fd62ade405a03a3a0c77c0
kristianinc <xankiiza@gmail.com>
2023-07-27 11:12:07 +0300

    event card corousel working

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -96,12 +96,31 @@
 
                             @if (count($onevent) > 0)
 
-                            <div class="owl-carousel testimonial-carousel position-relative">
-                                @foreach ($onevent as $photo)
-                                    <img src="{{ asset('images/events/' . $photo->photo) }}" alt="Project Photo">
-                                @endforeach
-                            </div>
+                            {{-- <img src="{{ asset('images/events/' . $photo->photo) }}" alt="Project Photo"> --}}
+                            
+                            <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                       
+                                       @foreach ($onevent as $photo)
+                                        
+                                          <div class="carousel-item active">
+                                            <img src="{{ asset('images/events/' . $photo->photo) }}" class="card-img-top" alt="...">
+                                          </div>
 
+                                          @endforeach
+
+                                        </div>
+                                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+                                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Previous</span>
+                                        </button>
+                                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+                                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                          <span class="visually-hidden">Next</span>
+                                        </button>
+                                      </div>
+
+                         
                             @else
                             <p>No photos available for this project.</p>
                             @endif
